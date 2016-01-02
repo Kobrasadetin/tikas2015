b6b5ccdcb1acfe86dcd1ef20ef00f758b2f2b7ea
Mikko Karjanmaa <mikko.karjanmaa@gmail.com>
2016-01-02 22:06:33 +0200

    typo

--- tikas2015/src/main/java/com/majesticbyte/Tikas2015Application.java
+++ tikas2015/src/main/java/com/majesticbyte/Tikas2015Application.java
@@ -1,4 +1,4 @@
-package com.majseticbyte;
+package com.majesticbyte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- tikas2015/src/test/java/com/majesticbyte/Tikas2015ApplicationTests.java
+++ tikas2015/src/test/java/com/majesticbyte/Tikas2015ApplicationTests.java
@@ -1,5 +1,6 @@
-package com.majseticbyte;
+package com.majesticbyte;
 
+import com.majesticbyte.Tikas2015Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
