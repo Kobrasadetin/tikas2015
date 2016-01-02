e5641d721f7d91d51654b4d4975e9e84c2cbd5b3
Mikko Karjanmaa <mikko.karjanmaa@gmail.com>
2016-01-02 21:57:56 +0200

    Spring

---
+++ tikas2015/src/main/java/com/majseticbyte/Tikas2015Application.java
@@ -0,0 +1,12 @@
+package com.majseticbyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tikas2015Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tikas2015Application.class, args);
+	}
+}
