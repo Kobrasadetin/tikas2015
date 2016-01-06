c5cb44efc1bcc0cefc31cff12f04e459fecde684
Mikko Karjanmaa <mikko.karjanmaa@gmail.com>
2016-01-06 14:46:23 +0200

    Staattiset sivut

--- tikas2015/src/main/java/com/majesticbyte/Tikas2015Application.java
+++ tikas2015/src/main/java/com/majesticbyte/Tikas2015Application.java
@@ -2,13 +2,21 @@ package com.majesticbyte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
  
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan
-public class Tikas2015Application {
+public class Tikas2015Application extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Tikas2015Application.class);
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(Tikas2015Application.class);
     }
